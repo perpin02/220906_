d7f7102460ce40ad61189e71bf3309d739e27d7b
perpin02 <fstarhw02@naver.com>
2022-09-06 10:48:39 +0900

    다이아찍기를완성

--- 220906/star.c
+++ 220906/star.c
@@ -2,12 +2,33 @@
 
 int main()
 {
-	int input = 0;
+	int n;
 
-	scanf_s("%d", &input);
+	scanf_s("%d", &n);
 
-	for (int i = 0; i < input; i++)
-		printf("**");
+
+
+
+	for (int i = n; i > 0; i--)
+	{
+		for (int j = i; j > 0; j--)
+			printf("*");
+		for (int k = 2 * n - 2 * i; k > 0; k--)
+			printf(" ");
+		for (int j = i; j > 0; j--)
+			printf("*");
+		printf("\n");
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i; j > 0; j--)
+			printf("*");
+		for (int k = 2 * n - 2 * i; k > 0; k--)
+			printf(" ");
+		for (int j = i; j > 0; j--)
+			printf("*");
+		printf("\n");
+	}
 
 	return 0;
 }
